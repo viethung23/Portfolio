14a987aa66c3ac8f3ac693bb43ebe784e01b8081
viethung <viethungdev23@gmail.com>
2024-05-31 19:15:54 +0700

    modify navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -85,7 +85,7 @@ function NavBar() {
 
             <Nav.Item>
               <Nav.Link
-                href="https://soumyajitblogs.vercel.app/"
+                href="https://www.hocduthu.com/"
                 target="_blank"
                 rel="noreferrer"
               >
@@ -93,16 +93,7 @@ function NavBar() {
               </Nav.Link>
             </Nav.Item>
 
-            <Nav.Item className="fork-btn">
-              <Button
-                href="https://github.com/soumyajit4419/Portfolio"
-                target="_blank"
-                className="fork-btn-inner"
-              >
-                <CgGitFork style={{ fontSize: "1.2em" }} />{" "}
-                <AiFillStar style={{ fontSize: "1.1em" }} />
-              </Button>
-            </Nav.Item>
+          
           </Nav>
         </Navbar.Collapse>
       </Container>
